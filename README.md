5e64e751dff56a57a3ad074a2328e35a4caa8d0a
CommonJasminOrange <jiandx67@gmail.com>
2019-09-08 16:52:15 +0800

    添加忒å帖子示

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 [Spring文档]https://spring.io/guides  
 [Spring Boot官方文档]https://docs.spring.io/spring-boot/docs/2.1.7.RELEASE/reference/html/boot-features-sql.html#boot-features-embedded-database-support  
 [Spring web]https://spring.io/guides/gs/serving-web-content  
+[Thymelaf官方文档]https://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html  
 [Es中文社区]https://elasticsearch.cn  
 [Github ssh key]https://help.github.com/en/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent  
 [Bootstrap文档]https://v3.bootcss.com  
@@ -12,4 +13,13 @@
 [Git]https://git-scm.com/download/win  
 [Visual Paradigm]https://www.visual-paradigm.com  
 [Lombok]https://www.projectlombok.org  
+
+## idea常用快捷键
+Ctrl＋E：显示最近编辑的文件列表  
+Ctrl＋F12：显示当前文件的结构  
+Ctrl＋P：显示方法的参数信息  
+Ctrl＋Alt＋V ：自动创建变量也就是=号前的变量定义  
+Ctrl+Alt+L：  格式化代码  
+ALT+INSERT：生成get/set 实现接口等  
+Alt+回车：导入包自动修正
 
--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -1,22 +1,30 @@
 package com.example.community.controller;
 
+import com.example.community.dto.QuestionDto;
 import com.example.community.mapper.UserMapper;
 import com.example.community.model.User;
+import com.example.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
+
 
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length !=0)
         for (Cookie cookie : cookies) {
@@ -30,6 +38,8 @@ public class IndexController {
             }
         }
 
+        List<QuestionDto> questionList = questionService.list();
+        model.addAttribute("question",questionList);
         return "index";
     }
 }
