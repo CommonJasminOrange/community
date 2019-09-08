dd13c8d0331b49edf91cbf988e6084f14ffe4a47
CommonJasminOrange <jiandx67@gmail.com>
2019-09-08 17:50:52 +0800

    添加分页ï功能，并分离导航栏â

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -1,6 +1,6 @@
 package com.example.community.controller;
 
-import com.example.community.dto.QuestionDto;
+import com.example.community.dto.PaginationDto;
 import com.example.community.mapper.UserMapper;
 import com.example.community.model.User;
 import com.example.community.service.QuestionService;
@@ -8,10 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -24,7 +24,10 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size
+                        ) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length !=0)
         for (Cookie cookie : cookies) {
@@ -38,8 +41,9 @@ public class IndexController {
             }
         }
 
-        List<QuestionDto> questionList = questionService.list();
-        model.addAttribute("question",questionList);
+        PaginationDto pagination = questionService.list(page,size);
+
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/com/example/community/service/QuestionService.java
+++ src/main/java/com/example/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.example.community.service;
 
+import com.example.community.dto.PaginationDto;
 import com.example.community.dto.QuestionDto;
 import com.example.community.mapper.QuestionMapper;
 import com.example.community.mapper.UserMapper;
@@ -22,9 +23,25 @@ public class QuestionService {
     @Autowired
     private  UserMapper userMapper;
 
-    public  List<QuestionDto> list() {
-       List<Question> questions = questionMapper.list();
+    public PaginationDto list(Integer page, Integer size) {
+
+        PaginationDto paginationDto = new PaginationDto();
+        Integer totalCount = questionMapper.count();
+        paginationDto.setPagination(totalCount,page,size);
+
+        if (page < 1){
+            page = 1;
+        }
+
+        if(page > paginationDto.getTotalPage()){
+            page = paginationDto.getTotalPage();
+        }
+
+
+        Integer offset = size * (page - 1);
+       List<Question> questions = questionMapper.list(offset,size);
        List<QuestionDto> questionDtoList = new ArrayList<>();
+
         for (Question question: questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDto questionDto = new QuestionDto();
@@ -33,6 +50,8 @@ public class QuestionService {
             questionDto.setUser(user);
             questionDtoList.add(questionDto);
         }
-        return questionDtoList;
+
+        paginationDto.setQuestions(questionDtoList);
+        return paginationDto;
     }
 }
